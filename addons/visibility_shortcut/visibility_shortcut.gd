f343b47945415c12e56e7bd346e325b08bfdaa34
Janders1800 <zenit1800@gmail.com>
2024-03-01 01:54:23 +0100

    Added plugin files

---
+++ addons/visibility_shortcut/visibility_shortcut.gd
@@ -0,0 +1,25 @@
+@tool
+extends EditorPlugin
+
+
+var undo_redo : EditorUndoRedoManager = get_undo_redo()
+
+
+func _shortcut_input(event: InputEvent) -> void:
+	EditorSettings
+	if event is InputEventKey and event.is_pressed() and Input.is_key_pressed(KEY_H):
+		var selected_nodes : Array[Node] = get_editor_interface().get_selection().get_selected_nodes()
+		
+		undo_redo.create_action("Set visible")
+		for node in selected_nodes:
+			if node.has_method("hide"):
+				if node.visible:
+					# Hide node.
+					undo_redo.add_do_property(node, "visible", false)
+					undo_redo.add_undo_property(node, "visible", true)
+				else:
+					# Show node.
+					undo_redo.add_do_property(node, "visible", true)
+					undo_redo.add_undo_property(node, "visible", false)
+		undo_redo.commit_action()
+
