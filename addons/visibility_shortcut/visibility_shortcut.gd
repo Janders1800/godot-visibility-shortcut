5b2d9e27eb7b53047d2ee34ccda1287121dcf5e5
Janders1800 <zenit1800@gmail.com>
2025-06-29 07:44:32 +0200

    Bugfixes and optimizations
    No longer searching all the nodes, only the parents and children necessaries

--- addons/visibility_shortcut/visibility_shortcut.gd
+++ addons/visibility_shortcut/visibility_shortcut.gd
@@ -1,7 +1,6 @@
 @tool
 extends EditorPlugin
 
-
 var undo_redo : EditorUndoRedoManager = get_undo_redo()
 
 
@@ -11,64 +10,90 @@ func _shortcut_input(event: InputEvent) -> void:
 		if (event.shift_pressed and event.alt_pressed) or event.ctrl_pressed:
 			return
 		
+		if event.alt_pressed: # Show all nodes
+			undo_redo.create_action("Set visible")
+			show_all_visible_nodes(get_editor_interface().get_edited_scene_root())
+			undo_redo.commit_action()
+			
+			get_viewport().set_input_as_handled()
+			return
+		
+		var selected_nodes : Array[Node] = get_editor_interface().get_selection().get_selected_nodes()
+		if selected_nodes.is_empty():
+			return
+		
 		undo_redo.create_action("Set visible")
 		
-		if event.alt_pressed: # Show all nodes
-			var all_nodes : Array[Node] = get_all_nodes(get_editor_interface().get_edited_scene_root())
-			for node in all_nodes:
-				if "visible" in node:
-					undo_redo.add_do_property(node, "visible", true)
-					undo_redo.add_undo_property(node, "visible", false)
+		if event.shift_pressed: # Hide unselected nodes
+			var keep_visible := get_nodes_to_keep_visible(selected_nodes)
+			hide_unselected_nodes(get_editor_interface().get_edited_scene_root(), keep_visible)
 			undo_redo.commit_action()
 			
 			get_viewport().set_input_as_handled()
 			return
 		
-		if event.shift_pressed: # Hide unselected nodes
-			var all_nodes : Array[Node] = get_all_nodes(get_editor_interface().get_edited_scene_root())
-			var selected_nodes : Array[Node] = get_editor_interface().get_selection().get_selected_nodes()
-			var unselected_nodes : Array[Node]
-			var forviden : Array[Node]
-			
-			for node in all_nodes:
-				for selected in selected_nodes:
-					if node.is_ancestor_of(selected) or node in selected_nodes:
-						undo_redo.add_do_property(node, "visible", true)
-						undo_redo.add_undo_property(node, "visible", false)
-						forviden.append(node)
-			
-			for node in all_nodes:
-				if "visible" in node and not node in selected_nodes:
-					if not node in forviden:
-						unselected_nodes.append(node)
-			
-			for node in unselected_nodes:
-				if "visible" in node:
-					undo_redo.add_do_property(node, "visible", false)
-					undo_redo.add_undo_property(node, "visible", true)
-			undo_redo.commit_action()
-			
-			get_viewport().set_input_as_handled()
-			return
 		
 		# Hide selected nodes
-		var selected_nodes : Array[Node] = get_editor_interface().get_selection().get_selected_nodes()
+		for selected in selected_nodes:
+			if not ("visible" in selected):
+				continue
+			
+			var root : Node = get_editor_interface().get_edited_scene_root()
+			if selected == root:
+				undo_redo.add_do_property(selected, "visible", !selected.visible)
+				undo_redo.add_undo_property(selected, "visible", selected.visible)
+				continue
+			
+			var current : Node
+			current = selected.get_parent()
+			
+			while current:
+				if "visible" in current and not selected.visible and not current in selected_nodes:
+					undo_redo.add_do_property(current, "visible", true)
+					undo_redo.add_undo_property(current, "visible", current.visible)
+				if current != root:
+					current = current.get_parent()
+				else:
+					break
+			
+			undo_redo.add_do_property(selected, "visible", !selected.visible)
+			undo_redo.add_undo_property(selected, "visible", selected.visible)
 		
-		for node in selected_nodes:
-			if "visible" in node:
-				undo_redo.add_do_property(node, "visible", !node.visible)
-				undo_redo.add_undo_property(node, "visible", node.visible)
 		undo_redo.commit_action()
 		
 		get_viewport().set_input_as_handled()
 
-func get_all_nodes(root_node: Node) -> Array[Node]:
-	var all_nodes : Array[Node]
-	recursive_get_nodes(root_node, all_nodes)
-	return all_nodes
 
-func recursive_get_nodes(node: Node, nodes: Array[Node]) -> void:
-	nodes.append(node)
+func show_all_visible_nodes(node : Node) -> void:
+	if "visible" in node:
+		undo_redo.add_do_property(node, "visible", true)
+		undo_redo.add_undo_property(node, "visible", node.visible)
+	var root : Node = get_editor_interface().get_edited_scene_root()
 	for child in node.get_children():
-		if child.owner == get_editor_interface().get_edited_scene_root():
-			recursive_get_nodes(child, nodes)
+		if child.owner == root:
+			show_all_visible_nodes(child)
+
+
+func get_nodes_to_keep_visible(selected_nodes: Array[Node]) -> Array[Node]:
+	var keep_visible : Array[Node]
+	var root := get_editor_interface().get_edited_scene_root()
+	for node in selected_nodes:
+		while node:
+			if node.owner == root or node == root:
+				undo_redo.add_do_property(node, "visible", true)
+				undo_redo.add_undo_property(node, "visible", node.visible)
+				keep_visible.append(node)
+			if node == root:
+				break
+			node = node.get_parent()
+	return keep_visible
+
+
+func hide_unselected_nodes(node: Node, keep_visible: Array[Node]) -> void:
+	if "visible" in node and not node in keep_visible:
+		undo_redo.add_do_property(node, "visible", false)
+		undo_redo.add_undo_property(node, "visible", node.visible)
+	var root := get_editor_interface().get_edited_scene_root()
+	for child in node.get_children():
+		if child.owner == root:
+			hide_unselected_nodes(child, keep_visible)
