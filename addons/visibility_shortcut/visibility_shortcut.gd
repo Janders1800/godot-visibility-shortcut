636d35f117ccac8718fd2ad4a8120eaf06da955b
Janders1800 <zenit1800@gmail.com>
2025-06-29 05:22:42 +0200

    Hide unselected fixes

--- addons/visibility_shortcut/visibility_shortcut.gd
+++ addons/visibility_shortcut/visibility_shortcut.gd
@@ -4,6 +4,7 @@ extends EditorPlugin
 
 var undo_redo : EditorUndoRedoManager = get_undo_redo()
 
+
 func _shortcut_input(event: InputEvent) -> void:
 	
 	if event is InputEventKey and event.pressed and event.keycode == KEY_H:
@@ -31,15 +32,15 @@ func _shortcut_input(event: InputEvent) -> void:
 			
 			for node in all_nodes:
 				for selected in selected_nodes:
-					if node.is_ancestor_of(selected) or selected.is_ancestor_of(node):
+					if node.is_ancestor_of(selected) or node in selected_nodes:
+						undo_redo.add_do_property(node, "visible", true)
+						undo_redo.add_undo_property(node, "visible", false)
 						forviden.append(node)
 			
 			for node in all_nodes:
 				if "visible" in node and not node in selected_nodes:
-					for selected in selected_nodes:
-						if not selected.is_ancestor_of(node) and not node.is_ancestor_of(selected):
-							if not node in forviden:
-								unselected_nodes.append(node)
+					if not node in forviden:
+						unselected_nodes.append(node)
 			
 			for node in unselected_nodes:
 				if "visible" in node:
@@ -60,7 +61,6 @@ func _shortcut_input(event: InputEvent) -> void:
 		undo_redo.commit_action()
 		
 		get_viewport().set_input_as_handled()
-
 
 func get_all_nodes(root_node: Node) -> Array[Node]:
 	var all_nodes : Array[Node]
